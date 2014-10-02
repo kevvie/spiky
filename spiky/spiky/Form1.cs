bca5714b8d1dea8fe893c8c107a0180198761a6e
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2014-10-02 12:12:58 +0200

    perfect mute button added

--- spiky/spiky/Form1.cs
+++ spiky/spiky/Form1.cs
@@ -7,11 +7,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace spiky
 {
     public partial class spikyStartUp : Form
     {
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
+        private const int WM_APPCOMMAND = 0x319;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
+            IntPtr wParam, IntPtr lParam);
+
         public spikyStartUp()
         {
             InitializeComponent();
@@ -29,7 +39,8 @@ namespace spiky
         
         private void muteButton_Click_1(object sender, EventArgs e)
         {
-            new MuteSound();
+            SendMessageW(this.Handle, WM_APPCOMMAND, this.Handle,
+                (IntPtr)APPCOMMAND_VOLUME_MUTE);
         }
 
         private void exitButton_Click(object sender, EventArgs e)
