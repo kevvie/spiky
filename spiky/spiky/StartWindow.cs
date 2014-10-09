aeabfec160c16ec9a05c71e476099d19001163ac
Cderijck <corne_derijck@hotmail.com>
2014-10-09 13:11:43 +0200

    Mute shit weggehaald

--- spiky/spiky/StartWindow.cs
+++ spiky/spiky/StartWindow.cs
@@ -13,15 +13,6 @@ namespace spiky
 {
     public partial class spikyStartUp : Form
     {
-        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
-        private const int APPCOMMAND_VOLUME_UP = 0xA0000;
-        private const int APPCOMMAND_VOLUME_DOWN = 0x90000;
-        private const int WM_APPCOMMAND = 0x319;
-
-        [DllImport("user32.dll")]
-        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg,
-            IntPtr wParam, IntPtr lParam);
-
         public spikyStartUp()
         {
             InitializeComponent();
