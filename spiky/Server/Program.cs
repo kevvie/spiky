df1c039e67123009a3391b40c3d82d9b3eb986dd
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2014-10-30 13:57:05 +0100

    hij doet iets

--- spiky/Server/Program.cs
+++ spiky/Server/Program.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using System.Net;
 using System.Net.Sockets;
-using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
 
 namespace Server
 {
@@ -22,12 +18,14 @@ namespace Server
             TcpListener listener = new System.Net.Sockets.TcpListener(localhost, 1330);
             listener.Start();
 
+            
             while (true)
             {
                 Console.WriteLine("Waiting for connection");
-
+                //AcceptTcpClient waits for a connection from the client
                 TcpClient client = listener.AcceptTcpClient();
-
+                //start a new thread to handle this connection so we can go back 
+                //to waiting for another client
                 Thread thread = new Thread(HandleClientThread);
                 thread.Start(client);
             }
@@ -41,40 +39,23 @@ namespace Server
             bool done = false;
             while (!done)
             {
-                string received = ReadTextMessage(client);
+                string received = ReadMessage(client);
                 Console.WriteLine("Received: {0}", received);
-                done = received.Equals("bye");
-                if (done) WriteTextMessage(client, "BYE");
-                else WriteTextMessage(client, "OK");
+                done = received.Equals("qwertyuiop");
+                if (done) SendResponse(client, "BYE");
+                else SendResponse(client, "OK");
 
             }
+
             client.Close();
             Console.WriteLine("Connection closed");
         }
 
-        public static string ReadTextMessage(TcpClient client)
+        private static string ReadMessage(TcpClient client)
         {
-
-            StreamReader stream = new StreamReader(client.GetStream(), Encoding.ASCII);
-            string line = stream.ReadLine();
-
-
-            return line;
-        }
-
-        public static void WriteTextMessage(TcpClient client, string message)
-        {
-            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
-            stream.WriteLine(message);
-            stream.Flush();
-        }
-
-        public static string ReadMessage(TcpClient client)
-        {
-
             byte[] buffer = new byte[256];
             int totalRead = 0;
-
+            //read bytes until stream indicates there are no more
             do
             {
                 int read = client.GetStream().Read(buffer, totalRead, buffer.Length - totalRead);
@@ -83,10 +64,12 @@ namespace Server
             } while (client.GetStream().DataAvailable);
 
             return Encoding.Unicode.GetString(buffer, 0, totalRead);
+
         }
 
-        public static void SendMessage(TcpClient client, string message)
+        private static void SendResponse(TcpClient client, string message)
         {
+            //make sure the other end decodes with the same format!
             byte[] bytes = Encoding.Unicode.GetBytes(message);
             client.GetStream().Write(bytes, 0, bytes.Length);
         }
--- spiky/Spiky/Form1.cs
+++ spiky/Spiky/Form1.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Spiky
 {
     public partial class Form1 : Form
     {
-
-
-
-
         private string sendText;
+        private TcpClient client;
         public Form1()
         {
             InitializeComponent();
+            client = new TcpClient("127.0.0.1", 1330);
+
         }
 
         private void sendButton_Click(object sender, EventArgs e)
@@ -29,6 +30,7 @@ namespace Spiky
                 outputTextBox.AppendText("Me: " + sendText + "\n");
                 outputTextBox.AppendText(" " + "\n");
             }
+            SendMessage(client, sendText);
         }
 
         private void serverButton_Click(object sender, EventArgs e)
@@ -40,7 +42,28 @@ namespace Spiky
         private void Form1_Load(object sender, EventArgs e)
         {
 
-        } 
+        }
+
+        private static void SendMessage(TcpClient client, string message)
+        {
+            //make sure the other end encodes with the same format!
+            byte[] bytes = Encoding.Unicode.GetBytes(message);
+            client.GetStream().Write(bytes, 0, bytes.Length);
+        }
+
+        private static string ReadResponse(TcpClient client)
+        {
+            byte[] buffer = new byte[256];
+            int totalRead = 0;
+            //read bytes until there are none left
+            do
+            {
+                int read = client.GetStream().Read(buffer, totalRead,
+                    buffer.Length - totalRead);
+                totalRead += read;
+            } while (client.GetStream().DataAvailable);
+            return Encoding.Unicode.GetString(buffer, 0, totalRead);
+        }        
 
 
     }
