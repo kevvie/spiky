c5f3550a73dc8632295c2b4a45fbcd962cbb2fa7
kvandenakkerveken <kevvie_jaguar@hotmail.com>
2014-10-30 12:45:10 +0100

    server added

--- spiky/Spiky/Form1.Designer.cs
+++ spiky/Spiky/Form1.Designer.cs
@@ -31,6 +31,9 @@
             this.sendButton = new System.Windows.Forms.Button();
             this.outputTextBox = new System.Windows.Forms.TextBox();
             this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // sendButton
@@ -45,13 +48,13 @@
             // 
             // outputTextBox
             // 
-            this.outputTextBox.Location = new System.Drawing.Point(13, 12);
+            this.outputTextBox.Location = new System.Drawing.Point(13, 60);
             this.outputTextBox.Multiline = true;
             this.outputTextBox.Name = "outputTextBox";
             this.outputTextBox.ReadOnly = true;
             this.outputTextBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.outputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.outputTextBox.Size = new System.Drawing.Size(765, 431);
+            this.outputTextBox.Size = new System.Drawing.Size(765, 383);
             this.outputTextBox.TabIndex = 1;
             // 
             // inputTextBox
@@ -62,16 +65,45 @@
             this.inputTextBox.Size = new System.Drawing.Size(684, 150);
             this.inputTextBox.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(618, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "connect to server";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(435, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 17);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Choose your chat name";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(797, 625);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.inputTextBox);
             this.Controls.Add(this.outputTextBox);
             this.Controls.Add(this.sendButton);
             this.Name = "Form1";
             this.Text = "Spiky Chat";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -82,6 +114,9 @@
         private System.Windows.Forms.Button sendButton;
         private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- spiky/Spiky/Form1.cs
+++ spiky/Spiky/Form1.cs
@@ -29,5 +29,10 @@ namespace Spiky
             }
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
